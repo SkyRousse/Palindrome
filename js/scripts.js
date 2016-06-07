f8a10362fa27c1f56112f02eb9ebe97a55c6931f
Caleb Stokka and Sky Rousse <sky@>
2016-06-07 14:45:16 -0700

    split user interface and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
+//Business logic
+var palindrome = function (word) {
+	var wordArr = word.split("");
+	var reversedArr = wordArr.slice().reverse();
+	word = wordArr.join('');
+	drow = reversedArr.join('');
+	if (word === drow) {
+	return true;
+	} else {
+	return false;
+	}
+};
+
+//user interface logic
 $(function() {
 	$("form").submit(function(event) {
   event.preventDefault();
 	var word = $("input#word").val();
-	var wordArr = word.split("");
-	var reversedArr = wordArr.slice();
-	reversedArr.reverse();
-	word = wordArr.join('');
-	drow = reversedArr.join('');
-	if (word === drow) {
-	alert("You have a palindrome")
-	} else {
-	alert("This is not a palindrome")
+	var answer =	palindrome(word);
+	if (answer === true) {
+		$("#arrayOutput").text("This is a palindrome");
+	}
+	else {
+		$("#arrayOutput").text("This is not a palindrome");
 	}
    });
 });/*This file is for your custom js.  All yours*/
